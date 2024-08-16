877a2576f6868e53993dbabf85660fd50cd8dc77
smilefab <57106872+smilefab@users.noreply.github.com>
2024-08-16 15:26:21 +0200

    changed network architecture

--- soulaween/pytorch/SoulaweenNNet.py
+++ soulaween/pytorch/SoulaweenNNet.py
@@ -8,7 +8,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class SoulaweenNNet(nn.Module):
+class SmallSoulaweenNNet(nn.Module):
     def __init__(self, game, args):
         # game params
         self.board_x, self.board_y = game.getBoardSize()
@@ -59,3 +59,54 @@ class SoulaweenNNet(nn.Module):
         v = self.fc4(s)
 
         return F.log_softmax(pi, dim=1), torch.tanh(v)
+
+class SoulaweenNNet(nn.Module):
+    def __init__(self, game, args):
+        super(SoulaweenNNet, self).__init__()
+        
+        # Game parameters
+        self.board_x, self.board_y = game.getBoardSize()
+        self.action_size = game.getActionSize()
+        self.args = args
+
+        # Convolutional layers
+        self.conv1 = nn.Conv2d(1, args.num_channels, 3, stride=1, padding=1)  # 4x4 -> 4x4
+        self.conv2 = nn.Conv2d(args.num_channels, args.num_channels, 3, stride=1, padding=1)  # 4x4 -> 4x4
+
+        # Batch normalization
+        self.bn1 = nn.BatchNorm2d(args.num_channels)
+        self.bn2 = nn.BatchNorm2d(args.num_channels)
+
+        # Fully connected layers
+        self.fc1 = nn.Linear(args.num_channels * 4 * 4, 128)  # 4x4 -> Flatten to 16*num_channels
+        self.fc_bn1 = nn.BatchNorm1d(128)
+
+        # Policy head
+        self.fc_policy = nn.Linear(128, self.action_size)
+
+        # Value head
+        self.fc_value = nn.Linear(128, 1)
+
+    def forward(self, s):
+        # Reshape the input to fit the convolutional layers
+        s = s.view(-1, 1, self.board_x, self.board_y)  # batch_size x 1 x 4 x 4
+        
+        # Convolutional layers with ReLU and Batch Normalization
+        s = F.relu(self.bn1(self.conv1(s)))  # batch_size x num_channels x 4 x 4
+        s = F.relu(self.bn2(self.conv2(s)))  # batch_size x num_channels x 4 x 4
+
+        # Flatten for the fully connected layer
+        s = s.view(-1, self.args.num_channels * 4 * 4)  # batch_size x (num_channels * 16)
+
+        # Fully connected layer with dropout
+        s = F.dropout(F.relu(self.fc_bn1(self.fc1(s))), p=self.args.dropout, training=self.training)  # batch_size x 128
+
+        # Policy head: produces a log-softmax distribution over actions
+        pi = self.fc_policy(s)  # batch_size x action_size
+        pi = F.log_softmax(pi, dim=1)
+
+        # Value head: produces a scalar value
+        v = self.fc_value(s)  # batch_size x 1
+        v = torch.tanh(v)
+
+        return pi, v
